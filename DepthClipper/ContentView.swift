3549a0ef25c84b47f045a93543f841bd7da0dd02
Ryosuke Akiyama <r@554.jp>
2022-01-21 13:35:40 +0900

    Initial commit

---
+++ DepthClipper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DepthClipper
+//
+//  Created by Ryosuke Akiyama on 2022/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DepthClipper/DepthClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthClipperApp.swift
+//  DepthClipper
+//
+//  Created by Ryosuke Akiyama on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DepthClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
